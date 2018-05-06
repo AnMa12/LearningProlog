fa5e504d0047bb5f3e3d0859f29b153837fdfabd
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-06 19:17:08 +0300

    laboratory 2 exercise 1

--- Laboratory 2/twoPointsDistance.pl
+++ Laboratory 2/twoPointsDistance.pl
@@ -1,5 +1,7 @@
 
-/* Define a predicate distance/3 to calculate 
-   the distance between two 2D points */
+/* 
+   Define a predicate distance/3 to calculate 
+   the distance between two 2D points 
+					*/
 
 distance((X1,Y1), (X2, Y2), X) - X is sqrt((X2-X1)  2 + (Y2-Y1)  2).
