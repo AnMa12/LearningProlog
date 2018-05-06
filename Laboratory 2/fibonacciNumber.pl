5ea4b563f9f8a8a1cf2e61503b13105ef36fda1c
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-06 18:14:39 +0300

    laboratory 2 exercise 3
    another method. still doesn t work

--- Laboratory 2/fibonacciNumber.pl
+++ Laboratory 2/fibonacciNumber.pl
@@ -5,3 +5,9 @@ fibAlg(Nr, A, B, X) :- Nr > 0,
                        B1 is X-0, 
                        Nr1 is Nr-1, 
                        fibAlg(Nr1, A1, B1, X1). 
+
+
+fib(Nr, X) :- fibAlg(Nr, 1, 1, X).
+fibAlg(Nr, A, B, X) :- Nr > 1, 
+    				   A is A+B,
+                       fibAlg(Nr-1, B, X, A). 
