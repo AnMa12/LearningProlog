abca4c7ad194293cd6a008a434b23c767aad72f4
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-06 17:39:18 +0300

    laboratory 2 exercise 3
    first attempt to resolve the fibonacci problem.
    but it doesn t work: fibAlg/4: Arguments are not sufficiently instantiated

---
+++ Laboratory 2/fibonacciNumber.pl
@@ -0,0 +1,7 @@
+fib(Nr, X) :- fibAlg(Nr, 1, 1, X).
+fibAlg(Nr, A, B, X) :- Nr > 0, 
+    				   X1 is A+B, 
+                       A1 is B-0, 
+                       B1 is X-0, 
+                       Nr1 is Nr-1, 
+                       fibAlg(Nr1, A1, B1, X1). 
