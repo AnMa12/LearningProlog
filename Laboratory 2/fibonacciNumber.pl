9964fbce56d283f409197217aa7f32a997f93eeb
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-06 19:15:44 +0300

    laboratory 2 exercise 3
    efficient method

--- Laboratory 2/fibonacciNumber.pl
+++ Laboratory 2/fibonacciNumber.pl
@@ -1,13 +1,18 @@
-fib(Nr, X) :- fibAlg(Nr, 1, 1, X).
-fibAlg(Nr, A, B, X) :- Nr > 0, 
-    				   X1 is A+B, 
-                       A1 is B-0, 
-                       B1 is X-0, 
-                       Nr1 is Nr-1, 
-                       fibAlg(Nr1, A1, B1, X1). 
 
+/*find nth fibonacci term*/
 
-fib(Nr, X) :- fibAlg(Nr, 1, 1, X).
-fibAlg(Nr, A, B, X) :- Nr > 1, 
-    				   A is A+B,
-                       fibAlg(Nr-1, B, X, A). 
+/*
+fib(0, 1) :- !.
+fib(1, 1) :- !.
+fib(N, Result) :- N1 is N - 1, N2 is N - 2, fib(N1, Result1), fib(N2, Result2), Result is Result1 + Result2.
+*/
+
+fib( 0, 1 ).
+fib( 1, 1 ).
+fib( Nr, X ) :- fibAlg( Nr, 0, 1, X ).
+
+fibAlg( 0, A, _, A ).
+fibAlg( Nr, A, B, X ) :- Nr > 0, 
+    				   	NewB is A + B,
+    					N is Nr - 1,
+                       	fibAlg( N, B, NewB, X ).
