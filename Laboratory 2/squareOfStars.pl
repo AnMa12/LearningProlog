73471f686270e7053bf833995e6ca49f14997606
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-06 17:04:22 +0300

    laboratory 2 exercise 2

--- Laboratory 2/squareOfStars.pl
+++ Laboratory 2/squareOfStars.pl
@@ -1,6 +1,9 @@
+
+% trying with local variable
 square :- square(5, '* ').
 % N - number, S -symbol
-square(N, S) :- count(0,N,S), square(N,S).
-% I - start, F - stop, S - symbol
+square(N, S) :- Indice is N.
+square(N, S) :- Indice > 0 count(0,N,S), Indice is Indice-1, square(N,S).
+square(N, S) :- Indice =< 0, Indice is N.
 count(I,F,S) :- I =< F, write(S), I2 is I+1, count(I2,F,S).
 count(I,F,S) :- I > F, nl.
