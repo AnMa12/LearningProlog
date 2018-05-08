4d1e271d513496a89cdb17cc45099c6ccaa3b822
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-08 22:31:23 +0300

    laboratory 3 exercise 1A

--- Laboratory3/all_a.pl
+++ Laboratory3/all_a.pl
@@ -1,4 +1,7 @@
-all_a( [ ] ) :- !.
-
+/* all_a( [ ] ) :- !.
 all_a( [ Head | Tail ] ) :- Head == a,
-    			    all_a( Tail ).
+    			    all_a( Tail ).*/
+
+% verify if a list cotains just a
+all_a([]).
+all_a([a|T]) :- all_a(T).
