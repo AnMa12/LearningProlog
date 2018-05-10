2f6f8fefcb5694f46d60b40f3868c9a49389dcf5
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-10 17:16:53 +0300

    done zebraPuzzle resolver
    program in prolog that resolves the zebraPuzzle

--- Laboratory 5/zebraPuzzle.pl
+++ Laboratory 5/zebraPuzzle.pl
@@ -13,18 +13,22 @@ Strada = [
   member(casa(_,   spaniol,      _,         caine, _,     _), Strada), %3
   member(casa(_,   _,            verde,     _,     cafea, _), Strada), %4
   member(casa(_,   ucrainean,    _,         _,     cafea, _), Strada), %5    
-  member(casa(x,   _,            bej,       _,     _,     _), Strada), %6
-  member(casa(la_dreapta(y,x),   _,            verde,     _,     _,     _), Strada), %6
+  member(casa(X,   _,            bej,       _,     _,     _), Strada), %6
+  member(casa(Y,   _,            verde,     _,     _,     _), Strada), %6
+    la_dreapta(Y,X), %6
   member(casa(_,   _,            _,         melci, _,     oldgold), Strada), %7
   member(casa(_,   _,            galbena,   _,     _,     kools), Strada), %8
   member(casa(3,   _,            _,         _,     lapte, _), Strada), %9
   member(casa(1,   norvegian,    _,         _,     _,     _), Strada), %10
-  member(casa(t,   _,            _,         vulpe, _,     _), Strada), %11
-  member(casa(langa(t,s),    _,            _,         _,     _,     chesterfields), Strada), %11
-  member(casa(a,   _,            _,         cal,   _,     _), Strada), %12
-  member(casa(  langa(a,b),   _,            _,         _,     _,     kools), Strada), %12
+  member(casa(T,   _,            _,         vulpe, _,     _), Strada), %11
+  member(casa(S,    _,            _,         _,     _,     chesterfields), Strada), %11
+    langa(T,S), %11
+  member(casa(A,   _,            _,         cal,   _,     _), Strada), %12
+  member(casa(B,   _,            _,         _,     _,     kools), Strada), %12
+  	langa(A,B), %12
   member(casa(_,   _,            _,         _,     suc,   luckystrike), Strada), %13
   member(casa(_,   japonez,      _,         _,     _,     parliaments), Strada), %14
-  member(casa(c,   _,            albastra,  _,     _,     _), Strada), %15
-  member(casa(langa(c,d),   norvegian,    _,         _,     _,     _), Strada), %15
+  member(casa(C,   _,            albastra,  _,     _,     _), Strada), %15
+  member(casa(D,   norvegian,    _,         _,     _,     _), Strada), %15
+    langa(C,D), %15
   member(casa(_,   PosesorZebra, _,         zebra, _,     _), Strada). %whatWeWant
