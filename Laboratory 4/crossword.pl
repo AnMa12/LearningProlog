cdd83a0f8fa654902e70f6b2444f943a2e9d7808
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-05-10 16:09:34 +0300

    laboratory 4 exercise 4
    crossword game

---
+++ Laboratory 4/crossword.pl
@@ -0,0 +1,20 @@
+
+/*Sase cuvinte din engleza
+abalone, abandon, anagram, connect, elegant, enhance
+trebuie aranjate ^ntr-un puzzle de cuvinte ^ncrucisate ca ^n gura.
+Aici gasiti o baza de cunostinte ce reprezinta un lexicon ce contine
+aceste cuvinte.
+Deniti un predicat crosswd/6 care calculeaza toate variantele ^n care
+puteti completa grila. Primele trei argumente trebuie sa e cuvintele pe
+verticala, de la st^anga la dreapta, (V1,V2,V3), iar urmatoarele trei
+argumente trebuie sa e cuvintele pe orizontala, de sus ^n jos (H1,H2,H3).
+Hint: Specicati ca V1, V2, V3, H1, H2, H3 sunt cuvinte care au
+anumite litere comune. Unde este cazul, folositi variabile anonime.*/
+
+% Crossword puzzle
+word(abalone,a,b,a,l,o,n,e).
+word(abandon,a,b,a,n,d,o,n).
+word(enhance,e,n,h,a,n,c,e).
+word(anagram,a,n,a,g,r,a,m).
+word(connect,c,o,n,n,e,c,t).
+word(elegant,e,l,e,g,a,n,t).
