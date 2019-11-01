c896e5a36a92f93c9bf3626ba16d10d39aabe2bc
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2019-11-01 16:53:38 +0200

    Update squareOfStars.pl

--- Laboratory 2/squareOfStars.pl
+++ Laboratory 2/squareOfStars.pl
@@ -18,4 +18,19 @@ do_lines(M,N,C) :- M > 0 , do_line(N,C), M1 is M-1, do_lines(M1,N,C).
 %Write a single line, consisting of N characters followed by a newline character.
 do_line(N, C) :- N > 0, write(C), N1 is N-1 , do_line(N1,C) ; N = 0, nl.
 
+% other solution
+squareNew(Digit, Character) :-
+    squareHelper(Digit, Character, Digit, Digit).
 
+squareHelper(Digit, Character, 0, J) :-
+    J > 0,
+    nl,
+    NewJ = J - 1,
+    squareHelper(Digit, Character, Digit, NewJ).
+
+squareHelper(Digit, Character, I, J) :-
+    J > 0,
+    I > 0,
+    write(Character),
+    NewI is I - 1,
+    squareHelper(Digit, Character, NewI, J).
